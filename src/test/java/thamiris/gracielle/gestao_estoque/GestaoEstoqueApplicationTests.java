c86bf84aaf0ccb35e172af5eda9ce6d206360f4f
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-10 08:41:10 -0300

    Primeiro commit - sistema petshop

---
+++ src/test/java/thamiris/gracielle/gestao_estoque/GestaoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package thamiris.gracielle.gestao_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
