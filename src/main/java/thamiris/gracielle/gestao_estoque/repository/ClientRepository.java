b3403ba0df4a192b717d16b0bd5734ee9d97c5e9
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-10 09:30:36 -0300

    refacto: correcao da classe para interface

--- src/main/java/thamiris/gracielle/gestao_estoque/repository/ClientRepository.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/repository/ClientRepository.java
@@ -3,5 +3,5 @@ package thamiris.gracielle.gestao_estoque.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import thamiris.gracielle.gestao_estoque.model.Client;
 
-public interface ClienteRepository extends JpaRepository <Client, Long> {
+public interface ClientRepository extends JpaRepository <Client, Long> {
 }
