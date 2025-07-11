c2b4151fb30a61a17fd50b86846e1f4be48b0fcc
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-11 14:05:41 -0300

    feat: Criacao da funcao deleteCliente da classe controller

--- src/main/java/thamiris/gracielle/gestao_estoque/controller/ClientController.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/controller/ClientController.java
@@ -13,18 +13,46 @@ public class ClientController {
 
     private ClientRepository clientRepository;
 
+    @PostMapping
+    public ResponseEntity<String> createClient(@RequestBody Client client) {
+        return ResponseEntity.ok("Cliente cadastrado!");
+    }
+
 
     @GetMapping
-    public List<Client> listAll(){
-        return  clientRepository.findAll();
+    public List<Client> listAll() {
+        return clientRepository.findAll();
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity listById(@PathVariable Long id){
+    public ResponseEntity listById(@PathVariable Long id) {
         return clientRepository.findById(id)
                 .map(client -> ResponseEntity.ok(client))
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateClient(@PathVariable Long id, @RequestBody Client client) {
+        clientRepository.findById(id)
+                .map(client1 -> {
+                    client.setNome(client.getNome());
+                    client.setTelefone(client.getTelefone());
+                    client.setEmail(client.getEmail());
+
+                    clientRepository.save(client);
+                    return ResponseEntity.ok("Cliente atualizado com sucesso");
+                });
+        return ResponseEntity.status(404).body("Cliente não encontrado");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteClient(@PathVariable Long id){
+            return clientRepository
+                    .findById(id)
+                    .map(client -> { clientRepository.delete(client);
+                        return ResponseEntity.ok("Exclusao realizada com sucesso");
+                    })
+                    .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
 }
