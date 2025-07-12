73f361acdde5f20b44a0a2e8d49f8e4202da16a9
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-11 23:57:28 -0300

    feat: adicionar método para buscar pets por dono no PetRepository

--- src/main/java/thamiris/gracielle/gestao_estoque/repository/PetRepository.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/repository/PetRepository.java
@@ -1,7 +1,12 @@
 package thamiris.gracielle.gestao_estoque.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import thamiris.gracielle.gestao_estoque.model.Client;
 import thamiris.gracielle.gestao_estoque.model.Pet;
 
+import java.util.List;
+
 public interface PetRepository  extends JpaRepository <Pet,Long> {
+
+    List<Pet> findByDono(Client dono);
 }
