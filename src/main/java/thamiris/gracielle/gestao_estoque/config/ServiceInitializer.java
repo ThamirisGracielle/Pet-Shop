0da6ae05cda61cd47770e25f5f7c8bf72d9fa2a8
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-14 14:18:00 -0300

    refactor: Alimentando banco de dados com alguns servicos

--- src/main/java/thamiris/gracielle/gestao_estoque/config/ServiceInitializer.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/config/ServiceInitializer.java
@@ -2,8 +2,7 @@ package thamiris.gracielle.gestao_estoque.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import thamiris.gracielle.gestao_estoque.model.Categoria;
-import thamiris.gracielle.gestao_estoque.model.PetShopService;
+import thamiris.gracielle.gestao_estoque.model.*;
 import thamiris.gracielle.gestao_estoque.repository.PetShopServiceRepository;
 
 import java.util.List;
@@ -22,7 +21,24 @@ public class ServiceInitializer implements CommandLineRunner {
         // Só cadastra se não existir nenhum serviço no banco
         if (petShopServiceRepository.count()==0) {
             List<PetShopService> servicosIniciais = List.of(
-                    new PetShopService(null, "Banho Pequeno", 40.00, Categoria.HIGIENE_ESTETICA)
+                    new PetShopService(null, "Banho", Especie.GERAL,null, Categoria.HIGIENE_ESTETICA, Porte.PEQUENO, 40.00),
+                    new PetShopService(null, "Banho", Especie.GERAL,null, Categoria.HIGIENE_ESTETICA, Porte.MEDIO, 55.00),
+                    new PetShopService(null, "Banho",Especie.GERAL,null, Categoria.HIGIENE_ESTETICA, Porte.GRANDE, 70.00),
+                    new PetShopService(null, "Banho", Especie.GERAL,null, Categoria.HIGIENE_ESTETICA, Porte.GIGANTE, 90.00),
+                    new PetShopService(null, "Tosa Higiênica", Especie.GERAL,null, Categoria.HIGIENE_ESTETICA, Porte.GERAL, 35.00),
+                    new PetShopService(null, "Limpeza de Ouvido",  Especie.GERAL,null, Categoria.HIGIENE_ESTETICA, Porte.GERAL, 20.00),
+                    new PetShopService(null, "Escovação de Dente",Especie.GERAL, null,  Categoria.HIGIENE_ESTETICA, Porte.GERAL, 90.00),
+                    new PetShopService(null, "Escovação de Dente",Especie.GERAL,null,  Categoria.HIGIENE_ESTETICA, Porte.GERAL, 90.00),
+                    new PetShopService(null, "Vacinação V10",Especie.CACHORRO, null, Categoria.SAUDE, Porte.GERAL, 90.00),
+                    new PetShopService(null, "vacinação V4",Especie.GATO,null,  Categoria.SAUDE, Porte.GERAL, 85.00),
+                    new PetShopService(null, "Antirrábica",Especie.GERAL, null,  Categoria.SAUDE, Porte.GERAL, 35.00),
+                    new PetShopService(null, "Vermifugação",Especie.GERAL,  null,Categoria.SAUDE, Porte.GERAL, 85.00),
+                    new PetShopService(null, "Consulta Veterinária",Especie.GERAL,  null,Categoria.SAUDE, Porte.GERAL, 120.00),
+                    new PetShopService(null, "Castração",Especie.GATO,  Gender.MACHO,Categoria.SAUDE, Porte.GERAL, 180.00),
+                    new PetShopService(null, "Castração",Especie.GATO,  Gender.FEMEA,Categoria.SAUDE, Porte.GERAL, 250.00),
+                    new PetShopService(null, "Castração",Especie.CACHORRO,  Gender.FEMEA,Categoria.SAUDE, Porte.GERAL, 300.00),
+                    new PetShopService(null, "Castração",Especie.CACHORRO,  Gender.MACHO,Categoria.SAUDE, Porte.GERAL, 450.00)
+
             );
 
 
