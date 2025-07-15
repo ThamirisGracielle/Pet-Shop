4672995f73da4d9225cbdeeff9148fdc4d162c4d
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-15 03:39:15 -0300

    feat: Criação de metodos

--- src/main/java/thamiris/gracielle/gestao_estoque/repository/AppointmentRepository.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/repository/AppointmentRepository.java
@@ -3,6 +3,13 @@ package thamiris.gracielle.gestao_estoque.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import thamiris.gracielle.gestao_estoque.model.Appointment;
 import thamiris.gracielle.gestao_estoque.model.Client;
+import thamiris.gracielle.gestao_estoque.model.Pet;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> findByPetId(Long petId);
+    List<Appointment> findByDate(LocalDateTime start, LocalDateTime end);
 }
