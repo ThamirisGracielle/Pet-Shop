0a925c1e60f1c6aa33f51a99bdef9c0b97f0beb7
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-16 15:05:23 -0300

    feat: Criacao da classe AppointmentDTO

--- src/main/java/thamiris/gracielle/gestao_estoque/DTO/AppointmentDTO.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/DTO/AppointmentDTO.java
@@ -1,4 +1,16 @@
 package thamiris.gracielle.gestao_estoque.DTO;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class AppointmentDTO {
+
+    private Long clientId;
+    private Long petId;
+    private Long serviceId;
+    private LocalDateTime dataHora;
+    private String appointmentStatus;
+
 }
