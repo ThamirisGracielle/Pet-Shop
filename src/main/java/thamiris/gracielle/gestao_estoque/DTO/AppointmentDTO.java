de6c1df8672b90f11a6f16d303117b6e078d1cac
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-16 18:09:36 -0300

    feat: Criacao do Atributo AppointmentId

--- src/main/java/thamiris/gracielle/gestao_estoque/DTO/AppointmentDTO.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/DTO/AppointmentDTO.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Data
 public class AppointmentDTO {
 
+    private Long AppointmentId;
     private Long clientId;
     private Long petId;
     private Long serviceId;
