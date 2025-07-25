7b25638a1c07b4669de8cbb3b0635f8d242a76d3
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-25 01:58:02 -0300

    feat: Implenta classe

--- src/main/java/thamiris/gracielle/gestao_estoque/DTO/ClientDTO.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/DTO/ClientDTO.java
@@ -1,4 +1,15 @@
 package thamiris.gracielle.gestao_estoque.DTO;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class ClientDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private List<PetDTO> pets;
+
 }
--- src/main/java/thamiris/gracielle/gestao_estoque/DTO/PetDTO.java
+++ src/main/java/thamiris/gracielle/gestao_estoque/DTO/PetDTO.java
@@ -1,4 +1,17 @@
 package thamiris.gracielle.gestao_estoque.DTO;
 
+import lombok.Data;
+import thamiris.gracielle.gestao_estoque.model.Species;
+
+import java.time.LocalDate;
+
+@Data
 public class PetDTO {
+    private Long id;
+    private String nome;
+    private Species especie;
+    private String raca;
+    private LocalDate idade;
+    private Long donoId;
+
 }
