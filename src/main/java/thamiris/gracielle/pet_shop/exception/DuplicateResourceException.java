fe442393b2c93a803909fd189aa60b29441b525a
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2026-03-07 15:41:11 -0300

    feat: criar exceções customizadas para tratamento de erros

---
+++ src/main/java/thamiris/gracielle/pet_shop/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package thamiris.gracielle.pet_shop.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
